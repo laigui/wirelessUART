04033d3031f0e3020f15bbd3d2fbd319938c12c9
Wei <futurewayne@gmail.com>
2017-05-25 18:41:12 +0800

    temp. update

--- src/protocol/znldProtocol.py
+++ src/protocol/znldProtocol.py
@@ -16,6 +16,13 @@ from libs.E32Serial import E32
 
 
 class Protocol(threading.Thread):
+    class LampControl:
+        BYTE_RESERVED = '\x00'
+        BYTE_ALL_OFF = '\x00'
+        BYTE_ALL_ON = '\x03'
+        BYTE_LEFT_ON = '\x01'
+        BYTE_RIGHT_ON = '\x02'
+
     def __init__(self, id, role='RC', timeout=5):
         threading.Thread.__init__(self)
         self.thread_stop = False
@@ -25,9 +32,9 @@ class Protocol(threading.Thread):
         self._id = id
         self._tx_frame_len = 0
         if self._role == 'RC':
-            self._rx_frame_len = 34
+            self._rx_frame_len = 36
         elif self._role == 'Node':
-            self._rx_frame_len = 21
+            self._rx_frame_len = 36
         if ISRPI:
             port = '/dev/ttyS0'
         else:
@@ -38,16 +45,16 @@ class Protocol(threading.Thread):
         except:
             logger.error('The serial %s open failed!' % port)
             raise
-        self._frame_header = '5555'
-        self._frame_no = '00'
-        self._frame_crc = '00000000'
+        self._frame_header = '\x55\x55'
+        self._frame_no = 0
+        self._frame_crc = ''
 
     def __del__(self):
         self.ser.close()
 
     def send_message(self, dest_id, message):
         if self._role == 'RC':
-            assert len(message) == 4, 'RC message length is not 4'
+            assert len(message) == 17, 'RC message length is not 17'
         if self._role == 'Node':
             assert len(message) == 17, 'Node message length is not 17'
         tx_str = self._frame_header + self._id + dest_id + self._frame_no + message
@@ -74,12 +81,12 @@ class Protocol(threading.Thread):
 if __name__ == "__main__":
     try:
         logging.basicConfig(level='DEBUG')
-        foo = Protocol(id='123')
+        foo = Protocol(id='\x11\x22\x33\x44\x55\x66')
         foo.setName('Thread receiving')
         foo.start()
         while True:
             sleep(1)
-            foo.send_message('11','hell')
+            foo.send_message('\xA1\xA2\xA3\xA4\xA5\xA6','hell')
             pass
     except KeyboardInterrupt:
         foo.stop()
