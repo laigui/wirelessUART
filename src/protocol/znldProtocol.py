ea071261ad7f4cbbac4d5f4f853209a63db8d963
Wei <futurewayne@gmail.com>
2017-06-04 13:34:29 +0800

    specify parameter n of ser.receive on purpose

--- src/protocol/znldProtocol.py
+++ src/protocol/znldProtocol.py
@@ -125,7 +125,7 @@ class Protocol(threading.Thread):
         rx_str = ''
         rx_len = self._rx_frame_len
         while not done:
-            rx_str = rx_str + self.ser.receive(rx_len)  # keep receiving until getting required bytes
+            rx_str = rx_str + self.ser.receive(n=rx_len)  # keep receiving until getting required bytes
             if not got_header:
                 index = rx_str.find(self.LampControl.FRAME_HEADER)
                 if index == -1:
