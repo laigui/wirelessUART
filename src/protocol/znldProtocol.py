30e353a967d3aa50ddc309f0af311660fcda2bc3
Wei <futurewayne@gmail.com>
2017-07-01 18:48:44 +0800

    add len info for crc error message

--- src/protocol/znldProtocol.py
+++ src/protocol/znldProtocol.py
@@ -165,7 +165,7 @@ class ThreadRx(threading.Thread):
                     got_header = False
                     rx_str = rx_str[2 :]
                     rx_len = 2
-                    logger.error('A frame is received: %s', binascii.b2a_hex(rx_str))
+                    logger.error('A frame is received (len= %d): %s', len(rx_str), binascii.b2a_hex(rx_str))
                     logger.error('Payload: %s', binascii.b2a_hex(str_payload))
                     logger.error('RX CRC: %s', binascii.b2a_hex(rx_crc))
                     logger.error('calculated CRC: %s', binascii.b2a_hex(crc))
